5f06ff5e068043acbe96fd237d8c3692a9a90bf9
Allan Juma <allancjuma@gmail.com>
2024-02-12 00:54:00 +0300

    working

--- server/src/controllers/book.js
+++ server/src/controllers/book.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { sendSMS } = require("../utils/sendsms");
 
 const book = async (req, res) => {
   const { firstname, lastname, email, phoneNumber } = req.body;
@@ -7,31 +6,27 @@ const book = async (req, res) => {
   if (!firstname || !lastname || !email || !phoneNumber)
     return res.status(500).json({ message: "invalid request" });
 
-  sendSMS("Hello, your booking has been received", phoneNumber);
-
-  // //consider using event emitters here
-  // const url = "http://localhost:5000/api/pay";
-  // await axios({
-  //   method: "POST",
-  //   url,
-  //   data: {
-  //     amount: 1000,
-  //     msisdn: phoneNumber,
-  //   },
-  // })
-  //   .then((response) => {
-  //     if (response.status == 200) {
-  //       res.status(200).json({
-  //         message: "Stk push sent for booking fee payment of 1000",
-  //       });
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.log("book", error);
-  //     res.status(500).json({ error });
-  //   });
-
-  res.status(200).json({ message: "Booking successful" });
+  //consider using event emitters here
+  const url = "http://localhost:5000/api/pay";
+  await axios({
+    method: "POST",
+    url,
+    data: {
+      amount: 10,
+      phone: phoneNumber,
+    },
+  })
+    .then((response) => {
+      if (response.status == 200) {
+        res.status(200).json({
+          message: "Stk push sent for booking fee payment",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log("book", error);
+      res.status(500).json({ error });
+    });
 };
 
 module.exports = { book };
